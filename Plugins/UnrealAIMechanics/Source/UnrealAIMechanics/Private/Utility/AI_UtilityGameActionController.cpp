9eaaa37a49289998e945102763667052c5f4428f
rfowlie <robertanthonyfowlie@gmail.com>
2025-08-14 17:18:55 -0700

    Preliminary Setup

---
+++ Plugins/UnrealAIMechanics/Source/UnrealAIMechanics/Private/Utility/AI_UtilityGameActionController.cpp
@@ -0,0 +1,41 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Utility/AI_UtilityGameActionController.h"
+#include "Utility/AI_UtilityGameActionHandler.h"
+
+
+void UAI_UtilityGameActionController::GetAllEvaluatedActions(TArray<FGameActionEvaluated>& OutEvaluatedActions)
+{
+	// Gather actions from each evaluator
+	for (auto* Handler : GameActionHandlers)
+	{
+		if (Handler)
+		{
+			TArray<FGameActionEvaluated> Evaluated;
+			Handler->GetEvaluatedActions(Evaluated);
+			OutEvaluatedActions.Append(Evaluated);
+		}
+	}
+}
+
+void UAI_UtilityGameActionController::GetEvaluatedActions(
+	TArray<FGameActionEvaluated>& OutEvaluatedActions,
+	TSubclassOf<UAI_UtilityGameActionHandler> HandlerClass)
+{
+	for (auto* Handler : GameActionHandlers)
+	{
+		if (Handler->IsA(HandlerClass))
+		{
+			Handler->GetEvaluatedActions(OutEvaluatedActions);
+		}
+	}
+}
+
+void UAI_UtilityGameActionController::Execute()
+{
+	TArray<FGameActionEvaluated> EvaluatedActions;
+	GetAllEvaluatedActions(EvaluatedActions);
+	const FGameActionEvaluated Action = SelectEvaluatedAction(EvaluatedActions);
+	Action.Execute();
+}
---
+++ Plugins/UnrealAIMechanics/Source/UnrealAIMechanics/Public/Utility/AI_UtilityGameActionController.h
@@ -0,0 +1,40 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "AI_UtilityGameActionController.generated.h"
+
+class UAI_UtilityGameActionHandler;
+struct FGameActionEvaluated;
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALAIMECHANICS_API UAI_UtilityGameActionController : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	// get evaluated actions from all stored handlers
+	UFUNCTION(BlueprintCallable)
+	void GetAllEvaluatedActions(TArray<FGameActionEvaluated>& OutEvaluatedActions);
+
+	// get evaluated action from specified handler
+	UFUNCTION(BlueprintCallable)
+	void GetEvaluatedActions(TArray<FGameActionEvaluated>& OutEvaluatedActions, TSubclassOf<UAI_UtilityGameActionHandler> HandlerClass);
+
+	UFUNCTION(BlueprintCallable)
+	FGameActionEvaluated SelectEvaluatedAction(const TArray<FGameActionEvaluated>& InEvaluatedActions);
+
+	// should run all logic to conclusion
+	UFUNCTION(BlueprintCallable)
+	void Execute();
+
+	
+protected:	
+	UPROPERTY(EditAnywhere, Instanced, BlueprintReadWrite)
+	TArray<UAI_UtilityGameActionHandler*> GameActionHandlers;
+};
